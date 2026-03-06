f5b45cfae84c77aac70555822872a1c7cab9c574
LucasHsCarmo <lucascarmo03@gmail.com>
2026-03-06 09:06:33 -0300

    feat.: new schemas - validate users

--- car_api/schemas/users.py
+++ car_api/schemas/users.py
@@ -1,3 +1,4 @@
+from typing import Optional, List
 from pydantic import BaseModel, EmailStr
 
 class UserSchema(BaseModel):
@@ -9,3 +10,11 @@ class UserPublicSchema(BaseModel):
     id: int
     username: str
     email: EmailStr
+
+class UserUpdateSchema(BaseModel):
+    username: Optional[str] = None
+    email: Optional[EmailStr] = None
+    password: Optional[str] = None
+
+class UserListPublicSchema(BaseModel):
+    users: List[UserPublicSchema]
